f2b9676571d578e3cd341cade16cc643e50a7f1e
joeyladerer <joeyladerer@me.com>
2022-08-04 00:33:52 -0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 
-import ExampleCRUD from './Main Pages/ExampleCRUD';
-import HostDashboard from './Main Pages/HostDashboard';
-import Landing from './Main Pages/Landing Page/Landing';
-import LogIn from './Main Pages/Auth/LogIn';
-import SignUp from './Main Pages/Auth/SignUp';
-import CreateEvent from './Main Pages/CreateEvent';
-import EventDetails from './Main Pages/EventDetails';
-import VotingPage from './Main Pages/Voting Page/VotingPage';
+import ExampleCRUD from './frontend/ExampleCRUD';
+import HostDashboard from './frontend/HostDashboard';
+import Landing from './frontend/Landing Page/Landing';
+import LogIn from './frontend/Auth/LogIn';
+import SignUp from './frontend/Auth/SignUp';
+import CreateEvent from './frontend/CreateEvent';
+import EventDetails from './frontend/EventDetails';
+import VotingPage from './frontend/Voting Page/VotingPage';
 
 function App() {
   return (
